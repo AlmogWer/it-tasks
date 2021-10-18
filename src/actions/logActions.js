2d76736eb5d1b7cdfa98133b10c0038c68b439e6
Almog Wertzberger <Almogish@gmail.com>
2021-10-18 17:29:10 +0300

    connecting redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,5 +1,5 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '.types';
-
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+//Get logs from server
 export const getLogs = () => async (dispatch) => {
   try {
     setLoading();
