0106283ab715af9804821043c8b133c00870ab95
Almog Wertzberger <Almogish@gmail.com>
2021-10-18 17:54:29 +0300

    add logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
 //Get logs from server
 export const getLogs = () => async (dispatch) => {
   try {
@@ -13,6 +13,32 @@ export const getLogs = () => async (dispatch) => {
     });
   } catch (err) {
     dispatch({ type: LOGS_ERROR, payload: err.response.data });
+  }
+};
+
+//Add New Log
+export const addLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.statusText,
+    });
   }
 };
 
