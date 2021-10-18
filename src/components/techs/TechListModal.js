e91819ecf4d30210ac4dd3cf3c3d9f0504e2b88c
Almog Wertzberger <Almogish@gmail.com>
2021-10-18 16:19:55 +0300

    Edit and Tech modal

---
+++ src/components/techs/TechListModal.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import AddTechModal from './AddTechModal';
+import TechItem from './TechItem';
+
+const TechListModal = () => {
+  const [techs, setTechs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getTechs();
+    //eslint-disable-next-line
+  }, []);
+
+  const getTechs = async () => {
+    setLoading(true);
+    const res = await fetch('/techs');
+    const data = await res.json();
+    setTechs(data);
+    setLoading(false);
+  };
+
+  return (
+    <div id='tech-list-modal' className='modal rounded'>
+      <div className='modal-content'>
+        <h4>Technician List</h4>
+        <ul className='collection rounded'>
+          {!loading &&
+            techs.map((tech) => <TechItem tech={tech} key={tech.id} />)}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TechListModal;
